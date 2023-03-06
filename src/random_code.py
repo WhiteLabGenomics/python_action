0f130f00d7d90bd3c4a428669932560937041ecd
oscar-defelice <oscar.defelice@gmail.com>
2023-03-06 16:05:47 +0100

    style: Add black compliant code

--- src/random_code.py
+++ src/random_code.py
@@ -1,14 +1,25 @@
-"""File for random things."""
+"""Example file for random things."""
 
 
 class Random:
+    """Example class for random things."""
+
     def __init__(self, number: int):
         """
         Random initialisation
-        :param numer: a random number
+
+        Parameters
+        ----------
+        number: int
+            a random number
         """
         self.number = number
 
     def get_values(self) -> int:
-        """Return random value"""
+        """Return random value
+
+        Returns
+        -------
+        int
+        A random integer number"""
         return self.number
