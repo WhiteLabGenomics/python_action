4c7f18bd1d4aeadb223b0df56223df4b46a46646
oscar-defelice <oscar.defelice@gmail.com>
2023-03-07 16:33:13 +0100

    fix: Correct coverage excluded path

--- scripts/lintage/coverage.sh
+++ scripts/lintage/coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 $PYTHON coverage run --source=${PATH_TO_CODE} -m pytest --disable-pytest-warnings ${PATH_TO_UNIT_TESTS}
-score=$($PYTHON coverage report | tail -n1 | sed -E "s/ +/ /g" | cut -f4 -d " " | sed "s/%//")
+score=$($PYTHON coverage report | tail -n1 | sed -E "s/ +/ /g" | cut -f6 -d " " | sed "s/%//")
 condition=$(echo "$score>=$COVERAGE_SCORE" | bc -l)
 echo "COVERAGE UNIT TEST : ${score} | ACCEPTABLE_SCORE : ${COVERAGE_SCORE}"
 if [[ $condition == "1" ]]; then
